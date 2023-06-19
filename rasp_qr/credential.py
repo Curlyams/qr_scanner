d04d0082c4a341fd020f81ba4805345449caca97
Curlyams <bcurly33@gmail.com>
2023-06-19 10:46:29 -0700

    removed credentials

--- rasp_qr/credential.py
+++
@@ -1,7 +0,0 @@
-username = 'bcurly@qrpractice'
-password = 'Chargers2495!'
-server = 'qrpractice.database.windows.net'
-database = 'qr practice'
-vnc_acc_pw= "eRVMN5Cn8*YEvp_"
-vnc_pw = '25RideCon95#'
-port = 1433
