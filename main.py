d111cf0696c983af0747b0d9abd2b734b1052ede
Curlyams <Bcurly33@gmail.com>
2023-06-20 14:39:29 -0700

    connection string change

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from util.datafunctions import *
 from util.datascanner import *
 from util.sqlclass import *
-from credentials.credential import server, database, username, password
+from credentials.credential import server, database, username, password, connection_string_windows
 
-connection_string = f"Driver={{ODBC Driver 18 for SQL Server}};Server=tcp:{server},1433;Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-scanner = DataScanner(server, database, username, password, connection_string,dsn =None)
+connection_string = connection_string_windows
+scanner = DataScanner(server, database, username, password, connection_string ,dsn=None)
 scanner.scan_data(upload_time=10)
 
